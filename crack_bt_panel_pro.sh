f16727e0cf41d918fbe612b326f7d01605b4957e
Molly Lau <windowsbeta@outlook.com>
2018-11-30 22:00:08 +0800

    Update crack_bt_panel_pro.sh

--- crack_bt_panel_pro.sh
+++ crack_bt_panel_pro.sh
@@ -8,14 +8,14 @@ red='\033[0;31m'
 plain='\033[0m'
 
 #检查是否为Root
-[ $(id -u) != "0" ] && { echo "${red}[错误]${plain} 你必须以 root 用户执行此安装程序"; exit 1; }
+[ $(id -u) != "0" ] && { echo -e "${red}[错误]${plain} 你必须以 root 用户执行此安装程序"; exit 1; }
 
 echo "欢迎安装宝塔面板Pro破解版！"
-echo "${red}[警告]"
+echo -e "${red}[警告]"
 echo "本程序系个人制作，具备宝塔面板5.9专业版的所有功能"
 echo "如有侵权，请联系作者在第一时间处理"
 echo "安装并试用后，请在24小时内卸载"
-echo "${yellow}[安装说明]"
+echo -e "${yellow}[安装说明]"
 echo "本脚本必须在完全干净的 CentOS/Debian/Ubuntu 系统上安装"
 echo "如已安装更高版本的宝塔面板，请先卸载高版本再安装"
 echo "如已安装其他种类的面板，或 LNMP 之类的运行环境、一键包，建议备份好数据，重装干净系统再安装"
@@ -23,11 +23,12 @@ echo "使用本脚本出现的任何不良后果，本人概不负责"
 echo "联系方式："
 echo "zhihu: https://www.zhihu.com/people/deepdarkfantastic"
 echo "email: net.core@outlook.com"
+echo ""
 
 #确认安装
 while [ "$go" != 'y' ] && [ "$go" != 'n' ]
 do
-    read -p "${yellow}[警告]${plain} 确定要安装吗？(y/n): " go;
+    read -p "确定要安装吗？(y/n): " go;
 done
 if [ "$go" = 'n' ];then
     exit;
@@ -90,13 +91,13 @@ install_btPanel_for_CentOS() {
 
 install_btPanel_for_APT() {
     wget -O install.sh https://raw.githubusercontent.com/leitbogioro/Crack_BT_Panel/master/install-ubuntu.sh && sudo bash install.sh
-	wget -O update.sh https://raw.githubusercontent.com/leitbogioro/Crack_BT_Panel/master/update_pro.sh && bash update.sh pro
+    wget -O update.sh https://raw.githubusercontent.com/leitbogioro/Crack_BT_Panel/master/update_pro.sh && bash update.sh pro
 }
 
 #破解步骤
 crack_bt_panel() {
     export Crack_file=/www/server/panel/class/common.py
-    echo '破解执行中...'
+    echo -e "${yellow}[注意] ${plain}破解执行中..."
     /etc/init.d/bt stop
     sed -i $'164s/panelAuth().get_order_status(None)/{\'status\': \True, \'msg\': {\'endtime\': 32503651199}}/g' ${Crack_file}
     touch /www/server/panel/data/userInfo.json
@@ -125,7 +126,7 @@ elif [[ ${OS} == 'Ubuntu' ]] || [[ ${OS} == 'Debian' ]]; then
 	crack_bt_panel
 fi
 
-echo "${green}[完成] ${plain}宝塔面板破解版已安装成功！"
+echo -e "${green}[完成] ${plain}宝塔面板破解版已安装成功！"
 echo "按脚本提供的后台入口、账号、密码，登录宝塔面板并使用！"
 rm -rf crack_bt_panel_pro.sh
 exit 1
