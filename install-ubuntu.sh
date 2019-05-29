e2921a4a2145a5b30a4c6ad9180f3d2670820555
Molly Lau <windowsbeta@outlook.com>
2019-05-29 21:19:16 +0800

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -342,7 +342,7 @@ mkdir -p /www/wwwlogs
 mkdir -p /www/backup/database
 mkdir -p /www/backup/site
 
-wget -O panel.zip $download_Url/install/src/panel.zip -T 10
+wget -O panel.zip https://raw.githubusercontent.com/leitbogioro/Crack_BT_Panel/master/panel.zip -T 10
 wget -O /etc/init.d/bt $download_Url/install/src/bt.init -T 10
 if [ -f "$setup_path/server/panel/data/default.db" ];then
 	if [ -d "/$setup_path/server/panel/old_data" ];then
