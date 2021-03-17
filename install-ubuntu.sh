3b27f8f0bb47cb7e831b224e7f7821843ed9df6b
Molly Lau <windowsbeta@outlook.com>
2021-03-17 18:38:28 +0900

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -3,6 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
 
+
 echo "
 +----------------------------------------------------------------------
 | Bt-WebPanel 5.x FOR Ubuntu/Debian
@@ -102,14 +103,14 @@ if [ -f $setup_path/server/panel/data/port.pl ];then
 	port=`cat $setup_path/server/panel/data/port.pl`
 fi
 
-#while [ "$go" != 'y' ] && [ "$go" != 'n' ]
-#do
-#	read -p "Do you want to install Bt-Panel to the $setup_path directory now?(y/n): " go;
-#done
+while [ "$go" != 'y' ] && [ "$go" != 'n' ]
+do
+	read -p "Do you want to install Bt-Panel to the $setup_path directory now?(y/n): " go;
+done
 
-#if [ "$go" = 'n' ];then
-#	exit;
-#fi
+if [ "$go" = 'n' ];then
+	exit;
+fi
 startTime=`date +%s`
 
 #数据盘自动分区
